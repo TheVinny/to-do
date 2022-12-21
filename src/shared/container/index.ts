588ae0e6bce9822ff6995ff943ee717796aa615a
vinny <vinny.vip07@gmail.com>
2022-12-21 15:02:54 -0300

    feat: adding UserRepository on container

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,1 +1,6 @@
-import {} from 'tsyringe';
+import { container } from 'tsyringe';
+
+import { IUserRepository } from '@modules/users/domain/interfaces/IUserRepository';
+import UserRepository from '@modules/users/infra/repository/UserRepository';
+
+container.registerSingleton<IUserRepository>('UserRepository', UserRepository);
