42b9569f8a95e44887beabca319de78d497a0975
vinny <vinny.vip07@gmail.com>
2022-12-21 17:18:09 -0300

    feat: apply createUserController on route

--- src/modules/users/infra/http/routes/userRoutes.ts
+++ src/modules/users/infra/http/routes/userRoutes.ts
@@ -1,5 +1,8 @@
 import { Router } from 'express';
+import CreateUserController from '../controllers/CreateUserController';
 
 const usersRouter: Router = Router();
+
+usersRouter.post('/', CreateUserController.execute);
 
 export default usersRouter;
