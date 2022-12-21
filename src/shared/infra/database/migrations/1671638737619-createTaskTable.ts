e9be8381f69eb006b4132636671bab1145f1e599
vinny <vinny.vip07@gmail.com>
2022-12-21 13:20:44 -0300

    feat: adding migrations tables

---
+++ src/shared/infra/database/migrations/1671638737619-createTaskTable.ts
@@ -0,0 +1,67 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class createTaskTable1671638737619 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'tasks',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'description',
+            type: 'varchar',
+          },
+          {
+            name: 'completed',
+            type: 'boolean',
+            default: false,
+          },
+          {
+            name: 'priority',
+            type: 'int',
+            default: 0,
+          },
+          {
+            name: 'user_id',
+            type: 'uuid',
+          },
+          {
+            name: 'tag_id',
+            type: 'uuid',
+          },
+          {
+            name: 'created_at',
+            type: 'date',
+            default: 'now()',
+          },
+        ],
+        foreignKeys: [
+          {
+            name: 'user_fk',
+            referencedTableName: 'users',
+            referencedColumnNames: ['id'],
+            columnNames: ['user_id'],
+            onDelete: 'CASCADE',
+          },
+          {
+            name: 'tag_fk',
+            referencedTableName: 'tags',
+            referencedColumnNames: ['id'],
+            columnNames: ['tag_id'],
+            onDelete: 'SET NULL',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('tasks');
+  }
+}
---
+++ src/shared/infra/database/migrations/1671639273345-createTagTable.ts
@@ -0,0 +1,34 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class createTagTable1671639273345 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'tags',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'title',
+            type: 'varchar',
+            isUnique: true,
+          },
+          {
+            name: 'color',
+            type: 'varchar',
+            isUnique: true,
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('tags');
+  }
+}
