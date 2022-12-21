9b6a7e0ce2668848140fe87dcb1b7a5fcaca1868
vinny <vinny.vip07@gmail.com>
2022-12-21 14:19:27 -0300

    feat: adding uuid extension postgres on migration

--- src/shared/infra/database/migrations/1671638737619-createTaskTable.ts
+++ src/shared/infra/database/migrations/1671638737619-createTaskTable.ts
@@ -2,6 +2,7 @@ import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
 export class createTaskTable1671638737619 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
     await queryRunner.createTable(
       new Table({
         name: 'tasks',
