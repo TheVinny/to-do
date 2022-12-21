94596453e716696d77270cfac888235f17750d04
vinny <vinny.vip07@gmail.com>
2022-12-21 17:15:32 -0300

    feat: adding express async error import

--- src/App.ts
+++ src/App.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import '@shared/infra/database';
 import '@shared/container';
+import 'express-async-errors';
 import Server from '@shared/infra/http/Server';
 
 const PORT = process.env.APP_PORT || 3001;
