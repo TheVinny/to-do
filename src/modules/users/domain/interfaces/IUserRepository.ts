da7ed1b9b77c7d178e35ffc0044f8749d12ae9be
vinny <vinny.vip07@gmail.com>
2022-12-21 14:31:54 -0300

    fix: update find methods returns

--- src/modules/users/domain/interfaces/IUserRepository.ts
+++ src/modules/users/domain/interfaces/IUserRepository.ts
@@ -1,8 +1,8 @@
 import { IUser } from './IUser';
 
 export interface IUserRepository {
-  findById(id: string): Promise<IUser>;
-  findByEmail(email: string): Promise<IUser>;
+  findById(id: string): Promise<IUser | undefined>;
+  findByEmail(email: string): Promise<IUser | undefined>;
   save(user: IUser): Promise<void>;
   remove(user: IUser): Promise<void>;
 }
