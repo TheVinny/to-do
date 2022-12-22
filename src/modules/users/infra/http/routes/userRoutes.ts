c8badddc3cf37913b567b0f8d207f91b289f749f
vinny <vinny.vip07@gmail.com>
2022-12-22 12:38:45 -0300

    feat: create get user route

--- src/modules/users/infra/http/routes/userRoutes.ts
+++ src/modules/users/infra/http/routes/userRoutes.ts
@@ -1,6 +1,7 @@
 import AuthMiddleware from '@shared/infra/middlewares/AuthMiddleware';
 import { Router } from 'express';
 import CreateUserController from '../controllers/CreateUserController';
+import GetUserByIdController from '../controllers/GetUserByIdController';
 import UpdateUserController from '../controllers/UpdateUserController';
 
 const usersRouter: Router = Router();
@@ -8,5 +9,6 @@ const usersRouter: Router = Router();
 usersRouter.post('/', CreateUserController.execute);
 usersRouter.use(AuthMiddleware);
 usersRouter.put('/', UpdateUserController.execute);
+usersRouter.get('/', GetUserByIdController.execute);
 
 export default usersRouter;
