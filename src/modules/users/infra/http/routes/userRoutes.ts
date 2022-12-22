d531092075bb625268108e55d9a8f4f6d852c06c
vinny <vinny.vip07@gmail.com>
2022-12-22 12:23:38 -0300

    fix: insert use auth middleware on user routes

--- src/modules/users/infra/http/routes/userRoutes.ts
+++ src/modules/users/infra/http/routes/userRoutes.ts
@@ -1,8 +1,12 @@
+import AuthMiddleware from '@shared/infra/middlewares/AuthMiddleware';
 import { Router } from 'express';
 import CreateUserController from '../controllers/CreateUserController';
+import UpdateUserController from '../controllers/UpdateUserController';
 
 const usersRouter: Router = Router();
 
 usersRouter.post('/', CreateUserController.execute);
+usersRouter.use(AuthMiddleware);
+usersRouter.put('/', UpdateUserController.execute);
 
 export default usersRouter;
