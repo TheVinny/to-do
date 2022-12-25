3f7481f90dd85337652cd09191169052aceed388
vinny <vinny.vip07@gmail.com>
2022-12-25 15:45:58 -0300

    feat: add auth route

--- src/shared/infra/http/routes/routes.ts
+++ src/shared/infra/http/routes/routes.ts
@@ -1,8 +1,10 @@
+import AuthRoute from '@modules/auth/infra/http/routes/AuthRoute';
 import usersRouter from '@modules/users/infra/http/routes/userRoutes';
 import { Router } from 'express';
 
 const router: Router = Router();
 
 router.use('/users', usersRouter);
+router.use('/auth', AuthRoute);
 
 export default router;
