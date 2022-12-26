ebf15f81d962df0aa331981980edac554f9f6eea
vinny <vinny.vip07@gmail.com>
2022-12-26 19:29:46 -0300

    feat: get one controller

---
+++ src/modules/tags/infra/http/controllers/GetOne.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from 'express';
+import { container, injectable } from 'tsyringe';
+import getOne from '@modules/tags/services/GetOne';
+
+class GetOneController {
+  async execute(req: Request, res: Response): Promise<Response> {
+    const { id } = req.params;
+
+    const getone = container.resolve(getOne);
+
+    const tag = await getone.execute(id);
+
+    return res.status(200).json(tag);
+  }
+}
+
+export default new GetOneController();
