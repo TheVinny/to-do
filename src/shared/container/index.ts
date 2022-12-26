f88b31a278a68d8da15a5d5b40662428bb118341
vinny <vinny.vip07@gmail.com>
2022-12-26 10:39:19 -0300

    feat: inserting TagRepository in container

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -2,5 +2,8 @@ import { container } from 'tsyringe';
 
 import { IUserRepository } from '@modules/users/domain/interfaces/IUserRepository';
 import UserRepository from '@modules/users/infra/repository/UserRepository';
+import { ITagRepository } from '@modules/tags/domain/interfaces/ITagRepository';
+import TagRepository from '@modules/tags/infra/repository/TagRepository';
 
 container.registerSingleton<IUserRepository>('UserRepository', UserRepository);
+container.registerSingleton<ITagRepository>('TagRepository', TagRepository);
