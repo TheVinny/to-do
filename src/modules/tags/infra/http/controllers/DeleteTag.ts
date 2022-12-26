8fbdcb5d6deae7ec7a9086b6a6cc5d0439487686
vinny <vinny.vip07@gmail.com>
2022-12-26 19:31:02 -0300

    feat: create delete tag controller

---
+++ src/modules/tags/infra/http/controllers/DeleteTag.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+import DeleteTag from '@modules/tags/services/DeleteTag';
+
+class DeleteTagController {
+  async execute(req: Request, res: Response): Promise<Response> {
+    const { id } = req.params;
+
+    const deletetag = container.resolve(DeleteTag);
+
+    await deletetag.execute(id);
+
+    return res.status(204);
+  }
+}
+
+export default new DeleteTagController();
