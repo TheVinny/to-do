ed5425efa3a7ae683af11c5d49ca3bce13f02a37
vinny <vinny.vip07@gmail.com>
2022-12-27 10:51:34 -0300

    feat: add user id on delete tag method

--- src/modules/tags/infra/http/controllers/DeleteTag.ts
+++ src/modules/tags/infra/http/controllers/DeleteTag.ts
@@ -4,11 +4,11 @@ import DeleteTag from '@modules/tags/services/DeleteTag';
 
 class DeleteTagController {
   async execute(req: Request, res: Response): Promise<Response> {
-    const { id } = req.params;
-
+    const { tag_id } = req.params;
+    const { id } = req.user;
     const deletetag = container.resolve(DeleteTag);
 
-    await deletetag.execute(id);
+    await deletetag.execute({ id, tag_id });
 
     return res.status(204).json();
   }
