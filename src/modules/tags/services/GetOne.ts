b73077959420d1e9bb4bf3e91df093233c9e8b3c
vinny <vinny.vip07@gmail.com>
2022-12-27 10:57:20 -0300

    feat: adding user id to authentication

--- src/modules/tags/services/GetOne.ts
+++ src/modules/tags/services/GetOne.ts
@@ -1,5 +1,6 @@
 import AppError from '@shared/errors/AppError';
 import { inject, injectable } from 'tsyringe';
+import { IGetOne } from '../domain/interfaces/IGetOne';
 import { ITagRepository } from '../domain/interfaces/ITagRepository';
 import { ITag } from '../domain/interfaces/ITags';
 
@@ -10,10 +11,14 @@ class getOneService {
     private tagRepository: ITagRepository,
   ) {}
 
-  async execute(id: string): Promise<ITag> {
-    const hasTag = await this.tagRepository.findById(id);
+  async execute({ tag_id, id }: IGetOne): Promise<ITag> {
+    const hasTag = await this.tagRepository.findById(tag_id);
 
     if (!hasTag) throw new AppError('Tag not found', 404);
+
+    if (hasTag.user.id !== id) {
+      throw new AppError('Tag not found', 404);
+    }
 
     return hasTag;
   }
