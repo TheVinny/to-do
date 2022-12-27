99318251af8f6bfc7d2c46f2397448e37f32ef9c
vinny <vinny.vip07@gmail.com>
2022-12-27 10:51:09 -0300

    feat: add user id on create tag method

--- src/modules/tags/infra/http/controllers/CreateTag.ts
+++ src/modules/tags/infra/http/controllers/CreateTag.ts
@@ -1,19 +1,23 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 import CreateTag from '@modules/tags/services/CreateTag';
+import { instanceToInstance } from 'class-transformer';
 
 class CreateTagController {
   async execute(req: Request, res: Response): Promise<Response> {
     const { title, color } = req.body;
+    const { id } = req.user;
 
     const createtag = container.resolve(CreateTag);
-
     const tag = await createtag.execute({
       color,
       title,
+      user_id: id,
     });
 
-    return res.status(201).json(tag);
+    return res
+      .status(201)
+      .json(instanceToInstance(tag, { excludePrefixes: ['user'] }));
   }
 }
 
