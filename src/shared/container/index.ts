6e7dbc1ada41fbb58ccc765343544782d04f2d08
vinny <vinny.vip07@gmail.com>
2022-12-26 21:14:32 -0300

    feat: inserting Task Repository on container

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -4,6 +4,9 @@ import { IUserRepository } from '@modules/users/domain/interfaces/IUserRepositor
 import UserRepository from '@modules/users/infra/repository/UserRepository';
 import { ITagRepository } from '@modules/tags/domain/interfaces/ITagRepository';
 import TagRepository from '@modules/tags/infra/repository/TagRepository';
+import TaskRepository from '@modules/tasks/infra/repository/TaskRepository';
+import { ITaskRepository } from '@modules/tasks/domain/interfaces/ITaskRepository';
 
 container.registerSingleton<IUserRepository>('UserRepository', UserRepository);
 container.registerSingleton<ITagRepository>('TagRepository', TagRepository);
+container.registerSingleton<ITaskRepository>('TaskRepository', TaskRepository);
