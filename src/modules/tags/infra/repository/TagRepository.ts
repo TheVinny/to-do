0281acae2786d37e39c8f60d4ec00107bcb03e11
vinny <vinny.vip07@gmail.com>
2022-12-27 10:54:56 -0300

    fix: change params type and find method

--- src/modules/tags/infra/repository/TagRepository.ts
+++ src/modules/tags/infra/repository/TagRepository.ts
@@ -2,7 +2,7 @@ import Tag from '../model/TagModel';
 import { ITag } from '@modules/tags/domain/interfaces/ITags';
 import { EntityRepository, getRepository, Repository } from 'typeorm';
 import { ITagRepository } from '@modules/tags/domain/interfaces/ITagRepository';
-import { ICreateTag } from '@modules/tags/domain/interfaces/ICreateTag';
+import { ISaveTag } from '@modules/tags/domain/interfaces/ICreateTag';
 
 @EntityRepository(Tag)
 export default class TagRepository implements ITagRepository {
@@ -32,20 +32,23 @@ export default class TagRepository implements ITagRepository {
   }
 
   async findById(id: string): Promise<ITag | undefined> {
-    const user = await this.repository.findOne(id);
-
-    return user;
-  }
-
-  async findAll(): Promise<ITag[]> {
-    const tags = await this.repository.find();
+    const tags = await this.repository.findOne(id, { relations: ['user'] });
 
     return tags;
   }
 
-  async save(tag: ICreateTag): Promise<ITag> {
-    const saveUser = this.repository.create(tag);
+  async findAll(user_id: string): Promise<ITag[]> {
+    const tags = await this.repository.find({
+      where: {
+        user: user_id,
+      },
+    });
 
+    return tags;
+  }
+
+  async save(tag: ISaveTag): Promise<ITag> {
+    const saveUser = this.repository.create(tag);
     return await this.repository.save(saveUser);
   }
   async deleteById(tag: ITag): Promise<void> {
