46487a6bf68f45d79695f3d60c89ddff5883cd07
vinny <vinny.vip07@gmail.com>
2022-12-27 10:48:08 -0300

    feat: insert 'ISaveTag' interface on file

--- src/modules/tags/domain/interfaces/ICreateTag.ts
+++ src/modules/tags/domain/interfaces/ICreateTag.ts
@@ -1,4 +1,13 @@
+import { IUser } from '@modules/users/domain/interfaces/IUser';
+
 export interface ICreateTag {
   title: string;
   color: string;
+  user_id: string;
+}
+
+export interface ISaveTag {
+  title: string;
+  color: string;
+  user: IUser;
 }
