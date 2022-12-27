b95a30b36352bb710f14cecbef3f55b46d8cfe35
vinny <vinny.vip07@gmail.com>
2022-12-27 09:49:50 -0300

    feat: insert task routes

--- src/shared/infra/http/routes/routes.ts
+++ src/shared/infra/http/routes/routes.ts
@@ -1,5 +1,6 @@
 import AuthRoute from '@modules/auth/infra/http/routes/AuthRoute';
 import TagRouter from '@modules/tags/infra/http/routes/TagRoutes';
+import taskRoute from '@modules/tasks/infra/http/routes/taskRoute';
 import usersRouter from '@modules/users/infra/http/routes/userRoutes';
 import { Router } from 'express';
 
@@ -8,5 +9,6 @@ const router: Router = Router();
 router.use('/users', usersRouter);
 router.use('/auth', AuthRoute);
 router.use('/tag', TagRouter);
+router.use('/task', taskRoute);
 
 export default router;
