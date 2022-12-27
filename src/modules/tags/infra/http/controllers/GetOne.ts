13687a8ce396f7d77fb07ad8bc14e7e9a0c39ad2
vinny <vinny.vip07@gmail.com>
2022-12-27 10:52:27 -0300

    feat: add user id on get all tag method and exclude user object in response

--- src/modules/tags/infra/http/controllers/GetOne.ts
+++ src/modules/tags/infra/http/controllers/GetOne.ts
@@ -1,16 +1,18 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 import getOne from '@modules/tags/services/GetOne';
-
+import { instanceToInstance } from 'class-transformer';
 class GetOneController {
   async execute(req: Request, res: Response): Promise<Response> {
-    const { id } = req.params;
-
+    const { tag_id } = req.params;
+    const { id } = req.user;
     const getone = container.resolve(getOne);
 
-    const tag = await getone.execute(id);
+    const tag = await getone.execute({ id, tag_id });
 
-    return res.status(200).json(tag);
+    return res
+      .status(200)
+      .json(instanceToInstance(tag, { excludePrefixes: ['user'] }));
   }
 }
 
