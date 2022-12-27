97b265a1449b1874c6f6427b67439b75406059df
vinny <vinny.vip07@gmail.com>
2022-12-27 10:52:00 -0300

    feat: add user id on get all tag method

--- src/modules/tags/infra/http/controllers/GetAll.ts
+++ src/modules/tags/infra/http/controllers/GetAll.ts
@@ -3,9 +3,10 @@ import { container } from 'tsyringe';
 import getAll from '@modules/tags/services/GetAll';
 
 class getAllTagController {
-  async execute(_req: Request, res: Response): Promise<Response> {
+  async execute(req: Request, res: Response): Promise<Response> {
+    const { id } = req.user;
     const getall = container.resolve(getAll);
-    const tags = await getall.execute();
+    const tags = await getall.execute(id);
     return res.status(200).json(tags);
   }
 }
