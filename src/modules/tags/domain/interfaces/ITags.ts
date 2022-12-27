8a699b3e2ff31fbe26772d14e01d1f963de0de3b
vinny <vinny.vip07@gmail.com>
2022-12-27 10:50:21 -0300

    feat: add user property

--- src/modules/tags/domain/interfaces/ITags.ts
+++ src/modules/tags/domain/interfaces/ITags.ts
@@ -1,8 +1,10 @@
 import Tasks from '@modules/tasks/infra/model/TasksModel';
+import User from '@modules/users/infra/model/UserModel';
 
 export interface ITag {
   title: string;
   id: string;
   color: string;
   task: Tasks;
+  user: User;
 }
