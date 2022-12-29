9ea76143132e43ce512553a18c78aff8aaeb4450
vinny <vinny.vip07@gmail.com>
2022-12-29 00:02:08 -0300

    feat: insert many to many relation task | TaskTag

--- src/modules/tasks/infra/model/TasksModel.ts
+++ src/modules/tasks/infra/model/TasksModel.ts
@@ -1,12 +1,14 @@
 import { ITag } from '@modules/tags/domain/interfaces/ITags';
 import Tag from '@modules/tags/infra/model/TagModel';
 import { ITasks } from '@modules/tasks/domain/interfaces/ITasks';
+import TasksTag from '@modules/task_tag/infra/model/TaskTag';
 import User from '@modules/users/infra/model/UserModel';
 
 import {
   Column,
   CreateDateColumn,
   Entity,
+  JoinColumn,
   ManyToOne,
   OneToMany,
   PrimaryGeneratedColumn,
@@ -32,7 +34,15 @@ class Tasks implements ITasks {
   @Column()
   completed: boolean;
 
+  @OneToMany(() => TasksTag, taskstag => taskstag.task, {
+    cascade: true,
+    eager: true,
+  })
+  @JoinColumn()
+  tasktag: TasksTag[];
+
   @ManyToOne(() => User, user => user.tasks)
+  @JoinColumn({ name: 'user_id' })
   user: User;
 
   @Column()
