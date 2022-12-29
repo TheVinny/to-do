5b2cd1c72e528043861591ad1964a5eb783f7cf6
vinny <vinny.vip07@gmail.com>
2022-12-28 23:50:13 -0300

    refactor: insert find interfaces tag on repository interface

--- src/modules/tags/domain/interfaces/ITagRepository.ts
+++ src/modules/tags/domain/interfaces/ITagRepository.ts
@@ -1,12 +1,19 @@
 import Tag from '@modules/tags/infra/model/TagModel';
 import { ISaveTag } from './ICreateTag';
+import {
+  IFindTagByColor,
+  IFindTagById,
+  IFindTagByTitle,
+  IFindTags,
+} from './IFindTags';
 import { ITag } from './ITags';
 
 export interface ITagRepository {
-  findById(id: string): Promise<ITag | undefined>;
-  findByTitle(title: string): Promise<ITag | undefined>;
-  findByColor(color: string): Promise<ITag | undefined>;
+  findById(id: IFindTagById): Promise<ITag | undefined>;
+  findByTitle(title: IFindTagByTitle): Promise<ITag | undefined>;
+  findByColor(color: IFindTagByColor): Promise<ITag | undefined>;
   deleteById(tag: Tag): Promise<void>;
   save(tag: ISaveTag): Promise<ITag>;
   findAll(user_id: string): Promise<ITag[]>;
+  findAllById(tags: IFindTags[]): Promise<ITag[]>;
 }
