6be2b1daa8d493f7c1a1c92cb60e68435ea6f5b6
vinny <vinny.vip07@gmail.com>
2022-12-29 00:02:56 -0300

    feat: create insert tag methods and insert user id on find queries

--- src/modules/tasks/infra/repository/TaskRepository.ts
+++ src/modules/tasks/infra/repository/TaskRepository.ts
@@ -1,29 +1,37 @@
 import Tasks from '../model/TasksModel';
-import { EntityRepository, getRepository, Repository } from 'typeorm';
+import { EntityRepository, getRepository, In, Repository } from 'typeorm';
 import { ITaskRepository } from '@modules/tasks/domain/interfaces/ITaskRepository';
 import { ITasks } from '@modules/tasks/domain/interfaces/ITasks';
 import { ISaveTask } from '@modules/tasks/domain/interfaces/ICreateTask';
+import { IInsertTag } from '@modules/tasks/domain/interfaces/IInsertTag';
+import TasksTag from '@modules/task_tag/infra/model/TaskTag';
+import { ITaskTag } from '@modules/task_tag/domain/interfaces/ITaskTag';
+import { IUser } from '@modules/users/domain/interfaces/IUser';
+import { IFindTagsInTask } from '@modules/tasks/domain/interfaces/IFindTagsInTask';
 
 @EntityRepository(Tasks)
 export default class TasksRepository implements ITaskRepository {
   private repository: Repository<Tasks>;
-
+  private taskTagRepository: Repository<TasksTag>;
   constructor() {
     this.repository = getRepository(Tasks);
+    this.taskTagRepository = getRepository(TasksTag);
   }
-  async findById(id: string): Promise<ITasks | undefined> {
+  async findById(id: string, user: IUser): Promise<ITasks | undefined> {
     const task = await this.repository.findOne({
       where: {
         id,
+        user,
       },
     });
 
     return task;
   }
-  async findByTag(tag_id: string): Promise<ITasks | undefined> {
+  async findByTag(tag_id: string, user: IUser): Promise<ITasks | undefined> {
     const task = await this.repository.findOne({
       where: {
         tags: tag_id,
+        user,
       },
     });
 
@@ -49,7 +57,6 @@ export default class TasksRepository implements ITaskRepository {
   }
 
   async save(task: ISaveTask): Promise<ITasks> {
-    console.log(task);
     const taskCreate = this.repository.create({
       ...task,
     });
@@ -59,8 +66,55 @@ export default class TasksRepository implements ITaskRepository {
     return taskCreate;
   }
 
-  async findAll(): Promise<ITasks[]> {
-    const tasks = await this.repository.find();
+  async insertTag({ tags, task, user }: IInsertTag): Promise<void> {
+    const tasktags: ITaskTag[] = [];
+
+    this.taskTagRepository.create();
+
+    tags.map(tag => {
+      const tasktag = this.taskTagRepository.create({
+        tag,
+        task,
+        user,
+      });
+
+      tasktags.push(tasktag);
+    });
+
+    await this.taskTagRepository.insert(tasktags);
+  }
+
+  async findAllTagInTask({
+    tags,
+    task,
+    user,
+  }: IFindTagsInTask): Promise<ITaskTag[]> {
+    const tagsId = tags.map(tag => tag.id);
+
+    const findTags = await this.taskTagRepository.find({
+      where: {
+        tag: In(tagsId),
+        task,
+        user,
+      },
+      loadEagerRelations: true,
+    });
+
+    return findTags;
+  }
+
+  async removeTags(tags: TasksTag[]): Promise<void> {
+    await this.taskTagRepository.remove(tags);
+  }
+
+  async findAll(user: IUser): Promise<ITasks[]> {
+    const tasks = await this.repository.find({
+      where: {
+        user,
+      },
+      relations: ['tasktag'],
+    });
+
     return tasks;
   }
 }
