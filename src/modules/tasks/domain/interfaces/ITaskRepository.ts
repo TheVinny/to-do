d612d42f2345e57b4f91ea55e47d3cdacc5a44f0
vinny <vinny.vip07@gmail.com>
2022-12-28 23:58:02 -0300

    refactor: insert user on TaskRepository interface props

--- src/modules/tasks/domain/interfaces/ITaskRepository.ts
+++ src/modules/tasks/domain/interfaces/ITaskRepository.ts
@@ -1,12 +1,19 @@
+import { IFindTagsInTask } from '@modules/tasks/domain/interfaces/IFindTagsInTask';
+import { ITaskTag } from '@modules/task_tag/domain/interfaces/ITaskTag';
+import { IUser } from '@modules/users/domain/interfaces/IUser';
 import { ISaveTask } from './ICreateTask';
+import { IInsertTag } from './IInsertTag';
 import { ITasks } from './ITasks';
 
 export interface ITaskRepository {
-  findById(id: string): Promise<ITasks | undefined>;
-  findByTag(tag_id: string): Promise<ITasks | undefined>;
+  findById(id: string, user: IUser): Promise<ITasks | undefined>;
+  findByTag(tag_id: string, user: IUser): Promise<ITasks | undefined>;
   priorityChange(task: ITasks): Promise<ITasks | undefined>;
+  findAllTagInTask({ task, user, tags }: IFindTagsInTask): Promise<ITaskTag[]>;
+  removeTags(tags: ITaskTag[]): Promise<void>;
   deleteById(task: ITasks): Promise<void>;
   completed(task: ITasks): Promise<void>;
-  findAll(): Promise<ITasks[]>;
+  findAll(user: IUser): Promise<ITasks[]>;
   save(task: ISaveTask): Promise<ITasks>;
+  insertTag(update: IInsertTag): Promise<void>;
 }
