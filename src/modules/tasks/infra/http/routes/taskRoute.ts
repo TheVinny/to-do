bff23966f390f690babb6e69ccf6d5ef924ddb9b
vinny <vinny.vip07@gmail.com>
2022-12-29 00:00:58 -0300

    feat: insert tag Task routes

--- src/modules/tasks/infra/http/routes/taskRoute.ts
+++ src/modules/tasks/infra/http/routes/taskRoute.ts
@@ -1,10 +1,16 @@
 import AuthMiddleware from '@shared/infra/middlewares/AuthMiddleware';
 import { Router } from 'express';
 import CreateTask from '../controllers/CreateTask';
+import GetAll from '../controllers/GetAll';
+import RemoveTagTask from '../controllers/RemoveTagTask';
+import ToggleTask from '../controllers/ToggleTask';
 
 const taskRoute: Router = Router();
 
 taskRoute.use(AuthMiddleware);
 taskRoute.post('/', CreateTask.execute);
+taskRoute.post('/tag', ToggleTask.execute);
+taskRoute.get('/', GetAll.execute);
+taskRoute.delete('/tag', RemoveTagTask.execute);
 
 export default taskRoute;
